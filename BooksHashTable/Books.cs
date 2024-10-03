2db7a530cdf62a6bd8468763dfb8c2b39636bc33
Анна <vindanka@gmail.com>
2024-10-04 00:58:11 +0300

    Create methods to manage library

--- BooksHashTable/Books.cs
+++ BooksHashTable/Books.cs
@@ -10,5 +10,44 @@ namespace BooksHashTable
         {
             books = new Hashtable();
         }
+
+        public void Add(int hash, string name)
+        {
+            books.Add(hash, name);
+            Console.WriteLine($"Book \"{name}\" was added to the library");
+        }
+
+        public void Remove(int hash)
+        {
+            if (books.ContainsKey(hash))
+            {
+                Console.WriteLine($"Book \"{books[hash]}\" was deleted from the library");
+                books.Remove(hash);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+
+        public void GetBook(int hash)
+        {
+            if(books.ContainsKey(hash))
+            {
+                Console.WriteLine($"Book : {books[hash]}");
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+
+        public void ShowAll()
+        {
+            foreach(var book in books.Values)
+            {
+                Console.WriteLine(book);
+            }
+        }
     }
 }

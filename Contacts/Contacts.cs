2634b596a84ced594a337df51b38b7c2891b2860
Анна <vindanka@gmail.com>
2024-10-03 20:47:19 +0300

    Create Contacts class

---
+++ Contacts/Contacts.cs
@@ -0,0 +1,11 @@
+﻿namespace Contacts
+{
+    public class Contacts
+    {
+        private Dictionary<string, long> contacts;
+        Contacts()
+        {
+            contacts = new Dictionary<string, long>();
+        }
+    }
+}

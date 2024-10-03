4ef60a5e28785a7779367818d03337e02fd87841
Анна <vindanka@gmail.com>
2024-10-03 20:58:10 +0300

    add simple methods to modify and show contacts

--- Contacts/Contacts.cs
+++ Contacts/Contacts.cs
@@ -1,4 +1,6 @@
-﻿namespace Contacts
+﻿using System.Xml.Linq;
+
+namespace Contacts
 {
     public class Contacts
     {
@@ -6,6 +8,38 @@
         Contacts()
         {
             contacts = new Dictionary<string, long>();
+        }
+
+        public void Add(string name, int phoneNumber)
+        {
+            contacts.Add(name, phoneNumber);
+            Console.WriteLine($"{phoneNumber} - {name} was added");
+        }
+
+        public void Remove(string name)
+        {
+            long phoneNumber;
+            if(contacts.TryGetValue(name, out phoneNumber))
+            {
+                contacts.Remove(name);
+                Console.WriteLine($"{phoneNumber} - {name} was deleted");
+            }
+        }
+
+        public long Get(string name)
+        {
+            long phoneNumber;
+            if (!contacts.TryGetValue(name,out phoneNumber))
+                Console.WriteLine("Not found");
+            return phoneNumber;
+        }
+
+        public void ShowAll()
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"{contact.Value } - {contact.Key}");
+            }
         }
     }
 }

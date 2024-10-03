95f189a10efb53a6340800af8f1369c6e900e958
Анна <vindanka@gmail.com>
2024-10-03 22:42:03 +0300

    Add simple methods to add person to queue, serve and count people

---
+++ BuyerQueue/Buyers.cs
@@ -0,0 +1,30 @@
+﻿namespace BuyerQueue
+{
+    public class Buyers
+    {
+        private Queue<string> buyers;
+
+        public Buyers()
+        {
+            buyers = new Queue<string>();
+        }
+
+        public void Add(string name)
+        {
+            buyers.Enqueue(name);
+            Console.WriteLine($"Buyer {name} was added");
+        }
+
+        public void Serve()
+        {
+            Console.WriteLine($"Serving {buyers.Peek}...");
+            buyers.Dequeue();
+            Console.WriteLine("Next buyer!");
+        }
+
+        public void Count()
+        {
+            Console.WriteLine($"{buyers.Count()} people in queue");
+        }
+    }
+}

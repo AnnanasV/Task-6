07003d1d632ced10bc772220659d14d431918cad
Анна <vindanka@gmail.com>
2024-10-04 00:27:37 +0300

    Create class for hashtable of books

---
+++ BooksHashTable/Books.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace BooksHashTable
+{
+    public class Books
+    {
+        private Hashtable books;
+
+        public Books()
+        {
+            books = new Hashtable();
+        }
+    }
+}

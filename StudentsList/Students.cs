a0d31464498d53143f1d85db142ac65ded91bae7
Анна <vindanka@gmail.com>
2024-10-03 22:24:56 +0300

    Create simple methods to modify and count students in list

--- StudentsList/Students.cs
+++ StudentsList/Students.cs
@@ -8,5 +8,35 @@
         {
             students = new List<string>();
         }
+
+        public void AddStudent(string student)
+        {
+            students.Add(student);
+        }
+
+        public void RemoveStudent(string student)
+        {
+            if(!students.Remove(student))
+            {
+                Console.WriteLine("Student not found");
+            }
+        }
+
+        public string GetStudent(string name)
+        {
+            string student;
+            student = students.Find(s => s.Contains(name, StringComparison.OrdinalIgnoreCase));
+            if(student == null)
+            {
+                Console.WriteLine("Not found");
+                student = string.Empty;
+            }
+            return student;
+        }
+
+        public void Count()
+        {
+            Console.WriteLine($"Students: {students.Count()}");
+        }
     }
 }
